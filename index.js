4206348462c3a2445a85361e193fb7376ef92d10
Tzheng456 <Tzheng456@gmail.com>
2021-07-24 02:06:39 +0000

    implemented sumItems in index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (let e of array) {
+    if (Array.isArray(e)) {
+      sum += sumItems(e);
+    } else {
+      sum += e;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
